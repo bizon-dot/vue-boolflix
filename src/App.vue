00107bc72707a3d3c9920b37d29a9b83eee5782e
robertoAureli <r.aureli@protonmail.com>
2021-09-24 15:19:55 +0200

    Effettua la ricerca tramite l'input e stampoa in console

--- src/App.vue
+++ src/App.vue
@@ -6,16 +6,31 @@
 </template>
 
 <script>
-import Header from './components/Header.vue'
+import Header from './components/Header.vue';
+import axios from 'axios';
 
 export default {
   name: 'App',
   components: {
     Header
   }, 
+  data(){
+    return {
+      endPoint: "https://api.themoviedb.org/3/search/movie?api_key=b088cb04b38937d2c60babc36cfd68ef&language=en-US&query=",
+    }
+  },
   methods: {
     searchFilm(inputText){
       console.log(inputText);
+      let uri = this.endPoint + inputText;
+      console.log(uri);
+       axios.get(uri).then(res => {
+          console.log(res.data);
+         
+         
+        }).catch(err => {
+          console.log("Error ", err);
+        })
     }
   }
 }
