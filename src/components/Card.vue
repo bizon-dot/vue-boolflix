2e8db597a5261cfd1bae0f2596af401e198badcd
robertoAureli <r.aureli@protonmail.com>
2021-09-28 11:26:42 +0200

    Milestone 3: aggiunta copertina album

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,19 +1,18 @@
 <template>
-  <li >
+  <li>
 
-    <!-- {{card}} -->
-
-   
-
-   {{card.title}}
+    {{card.title}}
 
     {{card.original_title}}
 
     {{card.vote_average}}
-    
+
     {{card.original_language}}
+
     <span v-if="card.original_language == 'en'" class="flag-icon flag-icon-gb-eng"></span>
-    <span v-bind:class="'flag-icon flag-icon-' + card.original_language"></span> 
+    <span v-else v-bind:class="'flag-icon flag-icon-' + card.original_language"></span> 
+    <br />
+    <img :src="'https://image.tmdb.org/t/p/w342' + card.backdrop_path">
 
   </li>
 
